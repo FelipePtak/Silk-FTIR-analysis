6afc667c2790dfd780f76380b44a966f803d1f02
FelipePtak <78236244+FelipePtak@users.noreply.github.com>
2024-08-30 17:29:39 +0300

    Update save_parameters.py
    Added filepath as function arguments. Using os module on the main function.

--- save_parameters.py
+++ save_parameters.py
@@ -12,18 +12,16 @@ def params_list(*params):
         wids.append(params[i+2])
     return amps, cens, wids
 
-def save_fitted_parameters(peaks, amps, cens, wids):
-    file = 'fitted_results.txt'
-    with open(file, 'w+') as f:
+def save_fitted_parameters(filepath, peaks, amps, cens, wids):
+    with open(filepath, 'w') as f:
         f.write("Original peak (1/cm)\tAmplitude (a.u.)\tFitted peak (1/cm)\tWidth (1/cm)\n")
         for (peak, amp, cen, wid) in zip(peaks, amps, cens, wids):
-            f.write(f"{peak}\t{amp}\t{cen}\t{wids}\n")
+            f.write(f"{peak : ^10}\t{amp : ^10}\t{cen : ^10}\t{wid : ^10}\n")
         f.close()
 
-def save_percentage(percent_dict: dict):
-    file = 'structures_formation.txt'
-    with open(file, 'w+') as f:
+def save_percentage(filepath, percent_dict: dict):
+    with open(filepath, 'w') as f:
         f.write('Structure\tPercentage\n')
         for (struc, val) in percent_dict.items():
-            f.write(f"{struc}\t{val:.2f}\n")
+            f.write(f"{struc : ^10}\t{val:.2f}\n")
         f.close()
