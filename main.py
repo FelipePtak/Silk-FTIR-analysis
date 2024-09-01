3cbf6508f8c8bb578551df0afddf740a731dc1d9
FelipePtak <78236244+FelipePtak@users.noreply.github.com>
2024-09-01 18:51:40 +0300

    Update main.py
    The area calculation is in a separated for loop, and now the main function outputs a summary of the secondary display for all the selected files.

--- main.py
+++ main.py
@@ -23,15 +23,30 @@ def main():
     print(f"Selected files: {file_path}")
     print("\n\n")
     files_list = list(file_path)
+
+    peaks = [1609, 1621, 1631, 1650, 1673, 1695, 1703]
+    assignments = [r'Intermolecular B-Sheet', r'Intermolecular B-Sheet', \
+    r'Intermolecular B-Sheet', r'A-Helix/Random Coils', 'B-Truns', \
+    r'Antiparallel amyloid B-Sheet', r'Antiparallel amyloid B-Sheet']
+
+    print("These are the peaks we want to fit:\n")
+    print(f'{'Peak (1/cm)' : ^10} | {'Assignment': ^10}\n')
+    for (peak, struc) in zip(peaks, assignments):
+        print(f"{peak : >5} | {struc: ^10}\n")
+
+    fit_dict = {} # This variable will have the filename as the keys and the popt as values
+
     for file in files_list:
         fullspectrum = loadData(file)
-        print("Full spectrum data: \n", fullspectrum.head())
+        print(f"Full spectrum data of: {os.path.basename(file)}\n")
+        fullspectrum.head()
 
         normalized_data = normalize_data(fullspectrum)
         plot_fulldata(normalized_data.wavenumber, normalized_data.normalized)
 
         amide_data = amide_band_data(normalized_data)
-        print("Amide I data: \n", amide_data.head())
+        print(f"Amide I data of {os.path.basename(file)}: \n")
+        amide_data.head()
         plot_amide(amide_data.wavenumber, amide_data.normalized)
 
         smoothed_data = smoothFilter(amide_data)
@@ -41,17 +56,7 @@ def main():
         corrected_data = baselineSubtract(smoothed_data)
         plot_filter(amide_baseline_sub, corrected_data)
 
-        peaks = [1609, 1621, 1631, 1650, 1673, 1695, 1703]
-        assignments = [r'Intermolecular B-Sheet', r'Intermolecular B-Sheet', \
-        r'Intermolecular B-Sheet', r'A-Helix/Random Coils', 'B-Truns', \
-        r'Antiparallel amyloid B-Sheet', r'Antiparallel amyloid B-Sheet']
-
-        print("These are the peaks we want to fit:\n")
-        print(f'{'Peak (1/cm)' : ^10} | {'Assignment': ^10}\n')
-        for (peak, struc) in zip(peaks, assignments):
-            print(f"{peak : >5} | {struc: ^10}\n")
-
-        print("Calculating initial guess for data fit...\n")
+        print(f"Calculating initial guess for {os.path.basename(file)} data fit...\n")
         initial_amplitudes = amplitudes_initial_guess(corrected_data, *peaks)
         initial_guess = build_initial_guess(peaks, initial_amplitudes)
         init_guess_file = 'initial_guess.txt'
@@ -66,12 +71,14 @@ def main():
 
         bounds = (lower_bounds, upper_bounds)
 
-        print("\nCalculating fit...")
+        print(f"\nCalculating fit for {os.path.basename(file)}...")
         popt, pcov = curve_fit(wrap_function,\
         corrected_data.wavenumber, corrected_data.normalized,\
         p0=initial_guess, bounds=bounds)
 
-        print(f"Fitted parameters: \n")
+        fit_dict[file] = popt
+
+        print(f"Fitted parameters for {os.path.basename(file)}: \n")
         for i in range(len(peaks)):
             print(f'Amplitude{i+1}: {popt[3*i] : ^10}')
             print(f'Peak{i+1}: {popt[3*i + 1] : ^10}')
@@ -104,12 +111,15 @@ def main():
         plt.legend()
         plt.show()
 
-        # Calculating areas
+    # Calculating areas
+    for (file, popt) in fit_dict.items():
 
         areas, total_area = areas_list(popt)
         helix_area, sheet_area, turn_area = struc_areas(areas)
         struc_list = [helix_area, sheet_area, turn_area]
+        print(f"Structural composition of {os.path.basename(file)}:")
         percentages = percentage_print(total_area, *struc_list)
+        print("\n")
 
         # Saving parameters into a file
 
