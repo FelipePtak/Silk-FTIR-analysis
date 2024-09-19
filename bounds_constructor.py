a29dc56076d4cca900d840bee1156ad0c7a5e549
FelipePtak <78236244+FelipePtak@users.noreply.github.com>
2024-09-19 16:24:17 +0300

    Update bounds_constructor.py
    Changed the width bounds. No longer from 0 to np.inf. Now it is from 5 to 25.

--- bounds_constructor.py
+++ bounds_constructor.py
@@ -1,11 +1,11 @@
-import numpy as np
+from module_imports import np
 
 def build_lower_bounds(peaks):
     lower_bounds = []
     for peak in peaks:
         lower_bounds.append(0)
         lower_bounds.append(peak - 2)
-        lower_bounds.append(0)
+        lower_bounds.append(5)
     return lower_bounds
 
 def build_upper_bounds(peaks):
@@ -13,5 +13,5 @@ def build_upper_bounds(peaks):
     for peak in peaks:
         upper_bounds.append(np.inf)
         upper_bounds.append(peak + 2)
-        upper_bounds.append(np.inf)
+        upper_bounds.append(25)
     return upper_bounds
